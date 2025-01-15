59ea29246732413d6b4511e0a06f5ecfa10c5bc1
Ankit from mac <ankitbudhori23@gmail.com>
2025-01-15 20:47:12 +0530

    first commit

---
+++ topbar.php
@@ -0,0 +1,13 @@
+<!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-primary navbar-dark ">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li>
+        <a class="nav-link text-white"  href="./" role="button"> <large><b>ilcslivelihood</b></large></a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
